5c67090af6d1cfce48ac1974b49945303ef5a210
wangpeng14 <wangpeng14@joyy.sg>
2021-11-15 18:00:03 +0800

    # add unique key.

--- finance/src/main/kotlin/com/yunfei/finance/service/salary/SalaryProcessingService.kt
+++ finance/src/main/kotlin/com/yunfei/finance/service/salary/SalaryProcessingService.kt
@@ -5,6 +5,7 @@ import com.yunfei.finance.dto.SalaryUploadDto
 import com.yunfei.finance.persistence.entity.Salary
 import com.yunfei.finance.service.persistence.MaterialPersistenceService
 import com.yunfei.finance.service.persistence.SalaryPersistenceService
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.text.DateFormat
@@ -19,29 +20,37 @@ class SalaryProcessingService(
 	val workLoadRatio: WorkLoadRatio
 ) {
 	fun salaryProcessing(dto: SalaryUploadDto): Salary {
-		val fmt: DateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-		val salaryRatio =
-			if (dto.workOvertime == true || dto.isWorkLoad()) workLoadRatio.getSalaryRatio() else BigDecimal.ONE
-		val material = materialPersistenceService.findById(dto.materialId) ?: throw Exception("未知的简易编号")
-		val salaryAmount =
-			salaryRatio.multiply(BigDecimal(dto.quantity)).multiply(material.unitPrice.toString().toBigDecimal())
-		var timeSeconds = BigDecimal(fmt.parse(dto.end).time).subtract(BigDecimal(fmt.parse(dto.start).time)).divide(BigDecimal("3600000"))
-		if (dto.launchDeduct == true) {
-			timeSeconds = timeSeconds.subtract(BigDecimal("0.5"))
+		return try {
+			val fmt: DateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+			val salaryRatio =
+				if (dto.workOvertime == true || dto.isWorkLoad()) workLoadRatio.getSalaryRatio() else BigDecimal.ONE
+			val material = materialPersistenceService.findById(dto.materialId) ?: throw Exception("未知的简易编号")
+			val salaryAmount =
+				salaryRatio.multiply(BigDecimal(dto.quantity)).multiply(material.unitPrice.toString().toBigDecimal())
+			var timeSeconds = BigDecimal(fmt.parse(dto.end).time).subtract(BigDecimal(fmt.parse(dto.start).time)).divide(BigDecimal("3600000"))
+			if (dto.launchDeduct == true) {
+				timeSeconds = timeSeconds.subtract(BigDecimal("0.5"))
+			}
+			val salary = Salary(
+				materialId = material.materialId,
+				staffId = dto.staffId,
+				salaryDate = dto.salaryDate,
+				start = dto.start,
+				end = dto.end,
+				launchDeduct = dto.launchDeduct ?: false,
+				quantity = dto.quantity,
+				subsidy = dto.subsidy,
+				workOvertime = dto.workOvertime ?: dto.isWorkLoad(),
+				salarySummary = salaryAmount,
+				workTime = timeSeconds
+			)
+			return salaryPersistenceService.save(salary)
+		} catch (e: Exception) {
+			if (e is DataIntegrityViolationException) {
+				throw Exception("重复输入!")
+			} else {
+				throw e
+			}
 		}
-		val salary = Salary(
-			materialId = material.materialId,
-			staffId = dto.staffId,
-			salaryDate = dto.salaryDate,
-			start = dto.start,
-			end = dto.end,
-			launchDeduct = dto.launchDeduct ?: false,
-			quantity = dto.quantity,
-			subsidy = dto.subsidy,
-			workOvertime = dto.workOvertime ?: dto.isWorkLoad(),
-			salarySummary = salaryAmount,
-			workTime = timeSeconds
-		)
-		return salaryPersistenceService.save(salary)
 	}
 }
